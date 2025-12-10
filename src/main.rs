8255697ba0ac490618b51181633482d0818e0d7c
mmythen <mjmythen@gmail.com>
2025-12-10 15:39:55 -0800

    circles bounce with gravity

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
 use macroquad::prelude::*;
 
 
-fn main() {
+struct Ball {
+    x: f32,
+    y: f32,
+    velocity: f32,
+    acceleration: f32,
+}
+
+#[macroquad::main("Gravity")]
+async fn main() {
+    let mut balls: Vec<Ball> = Vec::new();
+
     loop {
-        clear_background(BLACK);
+        clear_background(WHITE);
 
-        draw_circle(200.0, 200.0, 50.0, RED);
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (mouse_x, mouse_y) = mouse_position();
+            balls.push(Ball {
+                x: mouse_x,
+                y: mouse_y,
+                velocity: 1.0,
+                acceleration: 0.5
+            });
+        }
 
+        for ball in &mut balls {
+            ball.velocity += ball.acceleration;
+            ball.y += ball.velocity;
+
+            if ball.y > screen_height() - 15.0 {
+                ball.y = screen_height() - 15.0;
+                ball.velocity *= -0.8;
+
+                if ball.velocity > -0.01 {
+                    ball.acceleration = 0.0;
+                }
+            }
+
+            draw_circle(ball.x, ball.y, 10.0, BLACK);
+        }
+
+        next_frame().await;
     }
 }
